c2b4b0f78181ec5d06f204447a80db912f946806
TornikeL38 <120584873+TornikeL38@users.noreply.github.com>
2023-01-31 15:20:53 +0400

    Add homework 2
    add pop, push, shift, unshift.

---
+++ app.js
@@ -0,0 +1,14 @@
+let names = [
+        {name: "Tornike"},
+        {name:"Tato"},
+        {name:"Irakli"},
+        {name:"Lasha"},
+        {name:'Luka'},
+    ]
+
+names.pop();
+names.push({name:"Magda"});
+names.shift()
+names.unshift({name:"Gvanca"})
+console.log(names);
+
